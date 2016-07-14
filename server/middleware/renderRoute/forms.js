723e512f2f3baea4efa253dcfc3240c13d639563
MrEfrem <mr.efrem@gmail.com>
2016-07-14 23:34:12 +0400

    Add processing any form HTTP method (get, put, etc.), but need transfer URL in parameters form handler for match with requested form URL (string or RegExp).

--- server/middleware/renderRoute/forms.js
+++ server/middleware/renderRoute/forms.js
@@ -17,6 +17,16 @@ const addFormHandler = (arg) => {
   formsHandlers[arg.formName] = arg;
 };
 
+const checkPathMatch = (url, path) => {
+  if (url) {
+    if (url instanceof RegExp) {
+      return url.test(path);
+    }
+    return url === path;
+  }
+  return false;
+};
+
 const processingRequest = function* processingRequest(middlProps) {
   const requestFromFetchAPI = !this.request.headers.accept ||
     this.request.headers.accept.indexOf('text/html') === -1;
@@ -24,44 +34,51 @@ const processingRequest = function* processingRequest(middlProps) {
   const thisMiddlProps = middlProps;
   thisMiddlProps.componentProps = middlProps.componentProps;
   let needRenderPage = true;
-  // if contentType === application/x-www-form-urlencoded
-  if (this.request.headers['content-type'] &&
-    this.request.headers['content-type'].indexOf('application/x-www-form-urlencoded') !== -1) {
-    const forms = Object.keys(formsHandlers);
-    for (let i = 0; i < forms.length; i++) {
-      const formName = forms[i];
-      const propValidate = formsHandlers[formName];
-      if (this.request.body[formName] !== undefined &&
-        this.request.method.toLowerCase() === propValidate.method.toLowerCase()) {
-        // collect values form fields
-        thisMiddlProps.componentProps.initialValues = {};
-        Object.keys(this.request.body).forEach((param) => {
-          thisMiddlProps.componentProps.initialValues[param] = this.request.body[param];
-        });
-        thisMiddlProps.requestFromFetchAPI = requestFromFetchAPI;
-        const formErrors = propValidate.validate(this.request.body);
-        // Errors are found
-        if (Object.keys(formErrors).length) {
-          if (formsHandlers[formName].manualResponse) {
-            thisMiddlProps.componentProps.serverErrors = formErrors;
-            // Call registered Form handler
-            yield formsHandlers[formName].handler.call(this, thisMiddlProps);
-            needRenderPage = false;
-          } else {
-            // FetchAPI request. Return HTTP code 450
-            if (requestFromFetchAPI) {
-              statuses[450] = 'Error form data';
-              this.throw('Ошибки в данных формы', 450);
-            } else {
-              // JS off by client or JS errors (maybe old browser).
-              thisMiddlProps.componentProps.serverErrors = formErrors;
-            }
-          }
-        } else {
+  let formData;
+  if (this.request.method.toLowerCase() === 'get') {
+    formData = this.request.query;
+  } else {
+    formData = this.request.body;
+  }
+  const transferHeaderIsForm = this.request.headers['content-type'] &&
+    this.request.headers['content-type'].indexOf('application/x-www-form-urlencoded') !== -1;
+
+  const forms = Object.keys(formsHandlers);
+  for (let i = 0; i < forms.length; i++) {
+    const formName = forms[i];
+    const propValidate = formsHandlers[formName];
+    // Check transfer form
+    if (formData[formName] !== undefined &&
+      this.request.method.toLowerCase() === propValidate.method.toLowerCase() &&
+      (transferHeaderIsForm || checkPathMatch(propValidate.url, this.request.path))) {
+      // collect values form fields
+      thisMiddlProps.componentProps.initialValues = {};
+      Object.keys(formData).forEach((param) => {
+        thisMiddlProps.componentProps.initialValues[param] = formData[param];
+      });
+      thisMiddlProps.requestFromFetchAPI = requestFromFetchAPI;
+      const formErrors = propValidate.validate(formData);
+      // Errors are found
+      if (Object.keys(formErrors).length) {
+        if (formsHandlers[formName].manualResponse) {
+          thisMiddlProps.componentProps.serverErrors = formErrors;
           // Call registered Form handler
           yield formsHandlers[formName].handler.call(this, thisMiddlProps);
           needRenderPage = false;
+        } else {
+          // FetchAPI request. Return HTTP code 450
+          if (requestFromFetchAPI) {
+            statuses[450] = 'Error form data';
+            this.throw('Ошибки в данных формы', 450);
+          } else {
+            // JS off by client or JS errors (maybe old browser).
+            thisMiddlProps.componentProps.serverErrors = formErrors;
+          }
         }
+      } else {
+        // Call registered Form handler
+        yield formsHandlers[formName].handler.call(this, thisMiddlProps);
+        needRenderPage = false;
       }
     }
   }
